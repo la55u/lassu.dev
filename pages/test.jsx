db0c4104ebb38cf70b42635b101f0cb77fe47465
la55u <andras9405@gmail.com>
2021-09-11 16:55:59 +0200

    error handling

--- pages/test.jsx
+++ pages/test.jsx
@@ -2,6 +2,8 @@ import { Canvas } from "@react-three/fiber";
 import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 
+let sensor = null;
+
 function Box(props) {
   // This reference will give us direct access to the THREE.Mesh object
   const ref = useRef();
@@ -12,19 +14,40 @@ function Box(props) {
   // useFrame()
 
   useEffect(() => {
-    const sensor = new AbsoluteOrientationSensor({ frequency: 60 });
-    Promise.all([
-      navigator.permissions.query({ name: "accelerometer" }),
-      navigator.permissions.query({ name: "magnetometer" }),
-      navigator.permissions.query({ name: "gyroscope" }),
-    ]).then((results) => {
-      if (results.every((result) => result.state === "granted")) {
-        sensor.addEventListener("reading", handleSensor);
-        sensor.start();
+    try {
+      sensor = new AbsoluteOrientationSensor({ frequency: 60 });
+      sensor.addEventListener("error", (event) => {
+        // Handle runtime errors.
+        if (event.error.name === "NotAllowedError") {
+          // Branch to code for requesting permission.
+          Promise.all([
+            navigator.permissions.query({ name: "accelerometer" }),
+            navigator.permissions.query({ name: "magnetometer" }),
+            navigator.permissions.query({ name: "gyroscope" }),
+          ]).then((results) => {
+            if (results.every((result) => result.state === "granted")) {
+              console.log("Permission granted.");
+            } else {
+              console.log("No permissions to use AbsoluteOrientationSensor.");
+            }
+          });
+        } else if (event.error.name === "NotReadableError") {
+          console.log("Cannot connect to the sensor.");
+        }
+      });
+      sensor.addEventListener("reading", handleSensor);
+      sensor.start();
+    } catch (error) {
+      // Handle construction errors.
+      if (error.name === "SecurityError") {
+        // See the note above about feature policy.
+        console.log("Sensor construction was blocked by a feature policy.");
+      } else if (error.name === "ReferenceError") {
+        console.log("Sensor is not supported by the User Agent.");
       } else {
-        console.log("No permissions to use AbsoluteOrientationSensor.");
+        throw error;
       }
-    });
+    }
   }, []);
 
   function handleSensor(e) {
@@ -37,7 +60,7 @@ function Box(props) {
     <mesh
       {...props}
       ref={ref}
-      scale={active ? 1.5 : 1}
+      scale={active ? 2 : 1.5}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
