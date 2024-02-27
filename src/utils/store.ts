7d357482a71a45b6f1a56bde312fcd6baf2ccbad
Andras Lassu <andras.lassu@develops.hu>
2024-02-27 18:42:04 +0100

    nav links

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,26 +1,11 @@
 import { create } from "zustand";
 
-const DEFAULT_FORCE = -40;
-const DEFAULT_GRAVITY = -9.81;
-const STRONG_GRAVITY = 40;
-
 interface Store {
-  /** The force applied to each ball */
-  force: number;
-  gravity: number;
-
-  setForce: (f: number) => void;
-  resetForce: () => void;
-  setStrongGravity: () => void;
-  resetGravity: () => void;
+  nav: string;
+  setNav: (elementId: string) => void;
 }
 
-export const useSceneStore = create<Store>()((set) => ({
-  force: DEFAULT_FORCE,
-  gravity: DEFAULT_GRAVITY,
-
-  setForce: (f) => set({ force: f }),
-  resetForce: () => set({ force: DEFAULT_FORCE }),
-  setStrongGravity: () => set({ gravity: STRONG_GRAVITY }),
-  resetGravity: () => set({ gravity: DEFAULT_GRAVITY }),
+export const useGlobalStore = create<Store>()((set) => ({
+  nav: "",
+  setNav: (elementId) => set({ nav: elementId }),
 }));
